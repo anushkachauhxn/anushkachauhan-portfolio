62dd94a1eec8fa7e81093b69464217b102ad5d77
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-12-01 13:47:35 +0530

    Add pages for the router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
+import Home from "./pages/Home";
+import Work from "./pages/Work";
+import About from "./pages/About";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Home />,
+  },
+  {
+    path: "work",
+    element: <Work />,
+  },
+  {
+    path: "about",
+    element: <About />,
   },
 ]);
 
