e78b577f64cd86804bc8188fe67ded71ac801379
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-12-08 16:19:48 +0530

    Changes in row component to accomodate all pages

--- src/components/RowComponent.jsx
+++ src/components/RowComponent.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./RowComponent.css";
 
-function RowComponent({ page, image, title, description, link }) {
+function RowComponent({
+  page,
+  image,
+  title,
+  description,
+  links,
+  newTab = false,
+}) {
   return (
     <div className="row-container">
       <div className="img-box">
-        <a href={link}>
+        <a href={links[0]}>
           <img src={image} alt="" />
         </a>
       </div>
@@ -14,10 +20,22 @@ function RowComponent({ page, image, title, description, link }) {
       <div className="text-box">
         <h3 className="heading">{title}</h3>
         <p className="description">{description}</p>
+
         {page === "home" && (
-          <Link className="cta" to={link}>
+          <a href={links[0]} className="cta" target={newTab && "_blank"}>
             View Page
-          </Link>
+          </a>
+        )}
+
+        {page === "work" && (
+          <div className="links">
+            <a href={links[0]} className="cta" target="_blank">
+              View Live
+            </a>
+            <a href={links[1]} className="cta" target="_blank">
+              View Code
+            </a>
+          </div>
         )}
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,14 +21,15 @@ function Home() {
           image={image1}
           title="My Best Work"
           description="My best work listed here! Check out all the projects I've worked on. From challenging to tricky. From hair-pulling frustration to smug satisfaction."
-          link="/work"
+          links={["/work"]}
         />
         <RowComponent
           page="home"
           image={image2}
           title="My Last Internship"
           description="My experience at my most recent internship at Cope Studio, Polygon. Exciting projects, demanding deadlines and overall growth!"
-          link="https://medium.com/@anushkachauhan"
+          links={["https://medium.com/@anushkachauhan"]}
+          newTab={true}
         />
         <RowComponent
           page="home"
@@ -41,21 +42,22 @@ function Home() {
               Certifications I've achieved.
             </p>
           }
-          link="/profile"
+          links={["/profile"]}
         />
         <RowComponent
           page="home"
           image={image4}
           title="About Me"
           description="Get to know 'Anushka Chauhan' outside of work. Things I'm passionate about. The causes I support. My interests and hobbies."
-          link="/about"
+          links={["/about"]}
         />
         <RowComponent
           page="home"
           image={image5}
           title="View my CV"
           description="View and download my resume here!"
-          link={cv}
+          links={[cv]}
+          newTab={true}
         />
       </div>
 
