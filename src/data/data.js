e1804efff049ca72bf167bbaedb05876efa08b22
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-12-08 16:18:10 +0530

    Added data for best projects

---
+++ src/data/data.js
@@ -0,0 +1,72 @@
+import React from "react";
+import image1 from "../assets/work/1-rock-paper-scissors.jpg";
+import image2 from "../assets/work/2-sneakers-product-page.jpg";
+import image3 from "../assets/work/3-peppasauce.jpg";
+import image4 from "../assets/work/4-spotify-web.jpg";
+import image5 from "../assets/work/5-ip-address-tracker.jpg";
+
+export const data = {
+  best: [
+    {
+      index: 1,
+      image: image1,
+      title: "Rock Paper Scissors Lizard Spock",
+      description:
+        "Big Bang Theory's famous 'rock paper scissors lizard spock' game built using ReactJS, Redux and Material UI.",
+      links: [
+        "https://anushkachauhxn.github.io/fm-rock-paper-scissors-lizard-spock/",
+        "https://github.com/anushkachauhxn/fm-rock-paper-scissors-lizard-spock",
+      ],
+    },
+    {
+      index: 2,
+      image: image2,
+      title: "Sneakers Product Page",
+      description:
+        "The product page for a sneakers product with cart functionality and lightbox view.",
+      links: [
+        "https://anushkachauhxn.github.io/frontend-mentor-projects/projects/14-ecommerce-product-page/",
+        "https://github.com/anushkachauhxn/frontend-mentor-projects/tree/main/projects/14-ecommerce-product-page",
+      ],
+    },
+    {
+      index: 3,
+      image: image3,
+      title: "Peppasauce Website Clone",
+      description: "",
+      links: [
+        "https://peppasauce.netlify.app/",
+        "https://github.com/anushkachauhxn/my-peppesauce-love",
+      ],
+    },
+    {
+      index: 4,
+      image: image4,
+      title: "Spotify Web Redesign",
+      description: "",
+      links: [
+        "https://spotify-clone-anushkachauhxn.netlify.app/",
+        "https://github.com/anushkachauhxn/spotify-clone-react",
+      ],
+    },
+    {
+      index: 5,
+      image: image5,
+      title: "IP Address Tracker",
+      description:
+        "An application that tracks the physical address from an IP Address. Built using the IP Geolocation API and LeafletJS - A JavaScript library for interactive maps.",
+      links: [
+        "https://anushkachauhxn.github.io/frontend-mentor-projects/projects/8-ip-address-tracker/",
+        "https://github.com/anushkachauhxn/frontend-mentor-projects/tree/main/projects/8-ip-address-tracker",
+      ],
+    },
+  ],
+  all: [
+    {
+      index: 0,
+      title: "",
+      description: "",
+      links: [],
+    },
+  ],
+};
