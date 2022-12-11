eb2a69134966485db8c51b19ea3f845a96ab0d25
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-12-11 11:19:50 +0530

    Updated data

--- src/data/data.js
+++ src/data/data.js
@@ -65,10 +65,132 @@ export const data = {
   ],
   all: [
     {
-      index: 0,
-      title: "",
-      description: "",
-      links: [],
+      index: 1,
+      image: "",
+      title: "Order Summary Component",
+      description:
+        "This is component showing the user's order summary. It was built using Semantic HTML5 markup, CSS custom properties and CSS Flexbox.",
+      links: ["", ""],
+    },
+    {
+      index: 2,
+      image: "",
+      title: "FAQ Accordion Card",
+      description:
+        "This is an accordion card displaying FAQs for a platform. It was built using Bootstrap Framework along with Semantic HTML5 markup, CSS custom properties and CSS Flexbox.",
+      links: ["", ""],
+    },
+    {
+      index: 3,
+      image: "",
+      title: "Sunnyside Agency Landing Page",
+      description:
+        "This is a single page layout of an agency's website built using Semantic HTML5 markup, CSS custom properties and CSS Flexbox.",
+      links: ["", ""],
+    },
+    {
+      index: 4,
+      image: "",
+      title: "Pricing Component with Toggle Master",
+      description:
+        "This is a component showing a product's pricing plans. The user can toggle between Annual and Monthly plans. It was built using Semantic HTML5 markup, CSS custom properties and CSS Flexbox.",
+      links: ["", ""],
+    },
+    {
+      index: 5,
+      image: "",
+      title: "Testimonials Grid Section",
+      description:
+        "This is a page showing a company's testimonials in a grid fashion. It was built using Semantic HTML5 markup, CSS custom properties, CSS Grid and CSS Flexbox.",
+      links: ["", ""],
+    },
+    {
+      index: 6,
+      image: "",
+      title: "Interactive Pricing Component",
+      description:
+        "This is a pricing component that shows the prices corresponding to different amounts of pageviews. A toggle between monthly and yearly (with discounted rates) plans is also provided.",
+      links: ["", ""],
+    },
+    {
+      index: 7,
+      image: "",
+      title: "Fylo Data Storage Component",
+      description:
+        "This is a page showing a user's data storage. It was built using Semantic HTML5 markup, CSS custom properties and CSS Flexbox.",
+      links: ["", ""],
+    },
+    {
+      index: 8,
+      image: "",
+      title: "IP Address Tracker",
+      description:
+        "An application that tracks the physical address from an IP Address. It was built using the IP Geolocation API by IPify and LeafletJS - A JavaScript library for interactive maps.",
+      links: ["", ""],
+    },
+    {
+      index: 9,
+      image: "",
+      title: "Shortly URL Shortening API",
+      description:
+        "An application that shortens complete URLs. It was built using the shrtcode API and Vanilla JavaScript.",
+      links: ["", ""],
+    },
+    {
+      index: 10,
+      image: "",
+      title: "Chat App CSS Illustration",
+      description:
+        "A landing page showing the screen of a chat app and a small description. The mobile screen was completely designed with CSS only.",
+      links: ["", ""],
+    },
+    {
+      index: 11,
+      image: "",
+      title: "Launch Countdown Timer",
+      description:
+        "A page showing the countdown timer for an event. This was built using Vanilla JS and CSS Animations.",
+      links: ["", ""],
+    },
+    {
+      index: 12,
+      image: "",
+      title: "Bookmark Landing Page",
+      description:
+        "The landing page for a simple bookmark manager. The features and different versions of the product are listed there. This was built using HTML, CSS and Vanilla JS.",
+      links: ["", ""],
+    },
+    {
+      index: 13,
+      image: "",
+      title: "Room Homepage",
+      description:
+        "The landing page for a furniture brand with a slideshow to display its features. This was built using HTML, CSS and Vanilla JS.",
+      links: ["", ""],
+    },
+    {
+      index: 14,
+      image: "",
+      title: "E-commerce Product Page",
+      description:
+        "The product page for a sneakers product with cart functionality and lightbox view.",
+      links: ["", ""],
+    },
+    {
+      index: 15,
+      image: "",
+      title: "REST Countries API",
+      description:
+        "A web application that shows different countries of the world and their details with a colour theme switcher. Built using ReactJS.",
+      links: ["", ""],
+    },
+    {
+      index: 16,
+      image: "",
+      title: "Rock Paper Scissors Lizard Spock",
+      description:
+        "The Big Band Theory's 'rock paper scissors lizard spock' game built using ReactJS, Redux and Material UI.",
+      links: ["", ""],
     },
   ],
 };
